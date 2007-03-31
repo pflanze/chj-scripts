7b4b9624f413687d8620f6018d9db542cc71e2c8
root <root@elvis-5.(none)>
2007-03-31 19:56:13 +0200

    Added $installdir, $chroot_sh, chroot_sh_cat
    making use of the newly created ability of Chj::IO::Command to accept
    coderefs instead of executable paths

--- Chj/VserverSettings.pm
+++ Chj/VserverSettings.pm
@@ -22,6 +22,9 @@ package Chj::VserverSettings;
 @ISA="Exporter"; require Exporter;
 @EXPORT_OK=qw(
 	      $etcbase
+	      $installdir
+	      $chroot_sh
+	      chroot_sh_cat
 	     );
 %EXPORT_TAGS=(all=>\@EXPORT_OK);
 
@@ -29,5 +32,26 @@ use strict;
 
 our $etcbase= "/etc/vservers";
 
+# use Chj::xperlfunc;
+# sub chroot_sh {
+#     xsystem   hm nein, weil will sie ev ja auch  mit io pipes usw aufrufen.
+#       hm. das was im child ausgeführt werden muss hier so.  mal irgendwannnachdenken
+# }
+our $installdir= "/opt/vserver";
+our $chroot_sh= "$installdir/lib/util-vserver/chroot-sh";
+
+# should these be here?:
+use Chj::IO::Command;
+use Chj::xperlfunc qw(xchdir xexec);
+sub chroot_sh_cat {
+    @_ >= 1 or die "chroot_sh_cat: not enough arguments";
+    my $chrootdir= shift;
+    Chj::IO::Command->new_sender
+	(sub {
+	     xchdir $chrootdir; # is then being used by $chroot_sh as chroot dir
+	     xexec $chroot_sh, "cat", @_ #that's this sub's arguments, not directly the @_ from above (although the same values (passed through))
+	 },@_);
+}
+#(chroot_sh does (currently) not support any 'exists' or 'readlink' commands)
 
 1
