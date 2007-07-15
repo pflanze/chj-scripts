58de39970d37406e5f0497275ea3771c44a751ee
root <root@elvis-5.(none)>
2007-07-15 12:35:24 +0200

    (how the hell can this be?)
    (still same thing: exiting the parent leaves the child up.
     now because of the close STDIN; close STDOUT; close STDERR;
     ctl-d doesn't end the child anymore: the ssh logs out but hangs,
     ~. stuff in ssh will then lead to child termination.
     How can it be the child is waiting in <$r> when the writing end
      of that pipe cannot exist anywhere anymore? it's waiting there, right?
     How can it even be the child does have a handle to the ssh?
     )

--- Chj/Log.pm
+++ Chj/Log.pm
@@ -79,21 +79,27 @@ sub timedlogging_to_fh ($ $ ; $ ) {
     if (my $pid= xfork) {
 	# parent
 	xclose $r;
+	xclose $fh if $do_close;
 	my $wantarray=wantarray; # it's, deadly?, just so uuuugly. persistive. durchnichtive.
-	my @rv= $wantarray ? logging_to_fh($w, $thunk, $do_close) : scalar logging_to_fh($w, $thunk, $do_close);
+	my @rv= ( $wantarray ?
+		  logging_to_fh($w, $thunk, 1)
+		  : scalar logging_to_fh($w, $thunk, 1) );
 	xwaitpid $pid, 0;
 	$wantarray ? @rv : $rv[0]
     } else {
 	xclose $w;
+	close STDIN; close STDOUT; close STDERR;
 	# logger process
-	select $fh;
-	$|++;
+	{ my $oldfh= select;
+	  select $fh;
+	  $|++;
+	  select $oldfh; }
 	while (<$r>) {
 	    $_.="\\\n" unless /\n\z/; # for the last line of log.
 	    # copy from bin/log-timestamp:
 	    my ($seconds, $microseconds) = Time::HiRes::gettimeofday();
 	    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime($seconds);
-	    printf '%4d-%02d-%02d %02d:%02d:%02d.%06d %s',
+	    print $fh sprintf '%4d-%02d-%02d %02d:%02d:%02d.%06d %s',
 	      $year+1900, $mon, $mday, $hour,$min,$sec, $microseconds, $_
 		or die "timedlogging_to_fh child: printing to fh: $!";
 	    # /copy
