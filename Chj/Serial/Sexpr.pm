4bbf8d72f653cace21c7bee2b6c452113be085a7
root <root@elvis-5.(none)>
2007-07-17 11:15:05 +0200

    (Added, unfished (created recently))
     2007-07-13 18:49 Chj/Serial/Sexpr.pm
     2007-07-13 18:49 Chj/Serial/Sexpr/Parse.pm

---
+++ Chj/Serial/Sexpr.pm
@@ -0,0 +1,46 @@
+# Fri Jul 13 18:49:16 2007  Christian Jaeger, christian at jaeger mine nu
+# 
+# Copyright 2007 by Christian Jaeger
+# Published under the same terms as perl itself
+#
+# $Id$
+
+=head1 NAME
+
+Chj::Serial::Sexpr
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+
+=cut
+
+
+package Chj::Serial::Sexpr;
+#@ISA="Exporter"; require Exporter;
+#@EXPORT_OK=qw();
+#%EXPORT_TAGS=(all=>\@EXPORT_OK);
+
+use strict;
+
+use Class::Array -fields=>
+  -publica=>
+  ;
+
+
+# sub new {
+#     my $class=shift;
+#     my $s= $class->SUPER::new;
+#     (@$s[])=@_;
+#     $s
+# }
+
+#sub DESTROY {
+#    my $s=shift;
+#    local $@;
+#    # ç rausschmeissen wenn nicht benutzt, ebenso wie sub new
+#    $s->SUPER::DESTROY;
+#}
+
+end Class::Array;
