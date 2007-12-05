38783c025e67c9fa1f8efddfeab475e41c035dd8
root <root@elvis-mail.(none)>
2007-12-05 03:00:31 +0100

    Finished move (and hopefully fixing it)

--- Chj/is_if_up.pm
+++ Chj/is_if_up.pm
@@ -29,7 +29,7 @@ use strict;
 our $ifconfig= "/sbin/ifconfig";
 
 sub extractfromone {
-    my ($a)=@_; # string from ifconfig of one interface
+    my ($a,$if)=@_; # string from ifconfig of one interface
     if ($a=~ /^\s+UP\s/m) {
 	if ($a=~ /inet addr:(\d+\.\d+\.\d+\.\d+)\s+/) {
 	    return $1
@@ -59,33 +59,35 @@ sub is_if_up {
     my $flag_listmode;
     my $if= shift or wantarray ? $flag_listmode=1 : die "is_if_up: missing interface argument (like eth0 or eth1:1)";
     local $ENV{LANG}="C";
-    pipe is_if_up_RH,is_if_up_WH;# be careful about polluting the foreign namespace
-    my $pid=fork; die "is_if_up: could not fork" unless defined $pid;
+    pipe RH,WH or die "pipe: $!";
+    my $pid=fork;
+    die "is_if_up: could not fork: $!" unless defined $pid;
     if ($pid) {
-        close is_if_up_WH;
+        close WH;
         local $/=undef;
-        my $a= <is_if_up_RH>;
-        close is_if_up_RH;
+        my $a= <RH>;
+        close RH or die "close: $!";
         wait;
         if ($?) {
             warn "is_if_up: $a" unless $a=~ /device not found/i;## 13.4.04: todo auch dieses noch in ne exception umwandeln, oder nicht?
-            return undef; # probably "interface not found"; $? should still be readable outside
-        }
-	if ($flag_listmode) {
-	    # make list of arrayrefs:  [ "eth0", "..ip.." ], ...
-	    return map {
-		/^(\S+)/ or die "???: $_";
-		my $iface=$1;
-		my $ip= extractfromone $_;
-		[ $iface,$ip ]
-	    } split /\n{2,}/, $a
-	} else {
-	    return extractfromone $a
+            undef # probably "interface not found"; $? should still be readable outside
+        } else {
+	    if ($flag_listmode) {
+		# return list of arrayrefs:  [ "eth0", "..ip.." ], ...
+		map {
+		    /^(\S+)/ or die "???: '$_'";
+		    my $iface=$1;
+		    my $ip= extractfromone($_, $if);
+		    [ $iface,$ip ]
+		} split /\n{2,}/, $a
+	    } else {
+		extractfromone($a, $if)
+	    }
 	}
     } else {
-        close is_if_up_RH;
-        open STDOUT, ">&is_if_up_WH" or die;
-        open STDERR, ">&is_if_up_WH" or die;
+        close RH;
+        open STDOUT, ">&WH" or die $!;
+        open STDERR, ">&WH" or die $!;
         $flag_listmode ? exec $ifconfig : exec $ifconfig, $if;
         die "could not execute $ifconfig";
     }
