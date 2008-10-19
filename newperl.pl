e8ed17d108a0ee02f52b3d6040a88f1c4c96b7a6
root <root@novo.(none)>
2008-10-19 18:21:38 +0200

    Fix (and unify) path_or_namespace parsing
    So that baseclasses can also be given the same way as the
    to-be-created class (path, partial path/namespace, or namespace).

--- newperl.pl
+++ newperl.pl
@@ -56,4 +56,38 @@ sub edit {
 	}
 }
 
+{
+    package CHJ::Newperl::Path;
+    sub new {
+	my $cl=shift;
+	my ($path_or_ns)=@_;
+	# welches ist der pfad, welches der Namespace?
+	if ($path_or_ns=~ /(.*?)((?:\w+::)*\w+)(?:\.pm)?$/ ) {
+	    my ($path,$ns)=($1,$2);
+	    $path.=join("/",split(/::/,$ns));
+	    if ($ns!~ /::/ and $path!~ /^\//) {
+		# Benutzer hat wohl slashes statt :: verwendet.
+		$ns= $path;
+		$ns=~ s|^\./||;
+		# Assume that classes *always* start with an uppercase initial, remove lowercase path parts first:
+		$ns=~ s|^(?:[^A-Z/][^/]*/)+||s;
+		# Turn remaining path to namespace syntax
+		$ns=~ s|/|::|g;
+	    }
+	    $path.=".pm";
+	    bless {
+		   path=> $path,
+		   namespace=> $ns,
+		  }, $cl;
+	} else {
+	    #warn
+	    die "don't understand path or namespace '$path_or_ns'"; #can this happen at all? except with the empty string?
+	    #undef
+	}
+    }
+    sub path { $_[0]{path} }
+    sub namespace { $_[0]{namespace} }
+}
+
+
 1;
