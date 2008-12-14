5ec7abc00c6e3ab7b2a09d15cec5c2dc2371fe07
root <root@novo.(none)>
2008-12-14 01:22:03 +0100

    Add parse_schemestring

--- Chj/schemestring.pm
+++ Chj/schemestring.pm
@@ -19,7 +19,10 @@ Chj::schemestring
 
 package Chj::schemestring;
 @ISA="Exporter"; require Exporter;
-@EXPORT=qw(schemestring schemestring_oneline);
+@EXPORT=qw(schemestring
+	   schemestring_oneline
+	   parse_schemestring
+	  );
 
 use strict;
 
@@ -38,6 +41,28 @@ sub schemestring_oneline {
     $s=~ s/\r/\\r/sg;
     $s=~ s/\t/\\t/sg;
     "\"$s\""
+}
+
+sub parse_schemestring {
+    # expects the string including the leading and terminating double quotes
+    my ($s)=@_;
+    local $_=$s;
+    # strip leading and ending double quotes:
+    s/^.*?"//;
+    s/"[^"]*\z//;
+
+    # backslashes are 'difficult': one fresh backslash and a char is to be interpreted. ah, actually easy?:
+    s{\\(.)?}{
+        die "invalid schemestring ending with escaped end quote: '$s'"
+           unless defined $1;
+        (($1 eq '\\') ? '\\' : #yes it's different from sh where '\' would be the right thing
+         ($1 eq 'n') ? "\n" :
+         ($1 eq 'r') ? "\r" :
+         ($1 eq 't') ? "\t" :
+         ($1 eq '"') ? "\"" :
+         die "unfinished: unknown backslashed code sequence '$1' in: '$s'")
+    }seg;
+    $_
 }
 
 *Chj::schemestring= \&schemestring;
