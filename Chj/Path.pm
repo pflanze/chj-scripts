5dca5533c06133dd0b9f47f58f7578e8fc2a0f2a
Christian Jaeger (tie) <chrjae@gmail.com>
2011-09-09 00:17:33 -0400

    create

---
+++ Chj/Path.pm
@@ -0,0 +1,149 @@
+#
+# Copyright 2011 by Christian Jaeger, christian at jaeger mine nu
+# Published under the same terms as perl itself
+#
+# $Id$
+
+=head1 NAME
+
+Chj::Path
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+Not sure why I'm creating something from scratch here.
+
+Well, I just realize that Chj/Path/Calc.pm has broken handling of "//"
+I think.
+
+And where did I do it previously? Maildir?
+
+=cut
+
+
+package Chj::Path;
+
+use strict;
+
+use Class::Array -fields=>
+  -publica=>
+  'segments', # array of str not containing slashes
+  'has_endslash', # bool, whether the path is forcibly specifying a
+                  # dir by using a slash at the end (forcing a dir by
+                  # ending in "." isn't setting this flag)
+  'is_absolute', # bool
+  ;
+
+
+sub new {
+    my $cl=shift;
+    bless [@_], $cl
+}
+
+sub new_from_string {
+    my $cl=shift;
+    my ($str)=@_;
+    my @p= split m{/+}, $str;
+    $cl->new(\@p,
+	     scalar $str=~ m{/$}s,
+	     scalar $str=~ m{^/}s)
+}
+
+sub string_without_endslash {
+    my $s=shift;
+    join("/",@{$$s[Segments]})
+}
+
+sub string {
+    my $s=shift;
+    my $str= $s->string_without_endslash;
+    if ($s->has_endslash) {
+	if (length $str) {
+	    $str."/"
+	} else {
+	    if ($s->is_absolute) {
+		"/"
+	    } else {
+		# force using ".", XX hmm but yes no other way
+		"./"
+	    }
+	}
+    } else {
+	$str
+    }
+}
+
+# remove "." entries: (leave ".." in, because these cannot be resolved
+# without knowing the file system, right?)
+sub clean {
+    my $s=shift;
+    my $cl= ref $s;
+    bless [
+	   [
+	    grep {
+		not ($_ eq ".")
+	    } @{$$s[Segments]}
+	   ],
+	   # set forced dir flag if the last segment was a ".", even
+	   # if previously it didn't end in "/"
+	   ($$s[Has_endslash]
+	    or
+	    do {
+		if (defined (my $last= ${$$s[Segments]}[-1])) {
+		    $last eq "."
+		} else {
+		    0
+		}
+	    }),
+	   @$s[2..$#$s]
+	  ], $cl;
+}
+
+end Class::Array;
+
+__END__
+
+  tests:
+calc> :l (new_from_string Chj::Path "hello//world/you")->string
+hello/world/you
+calc> :l (new_from_string Chj::Path "/hello//world/you")->string
+/hello/world/you
+calc> :l (new_from_string Chj::Path "/hello//world/you/")->string
+/hello/world/you/
+calc> :l (new_from_string Chj::Path "/")->string
+/
+calc> :l (new_from_string Chj::Path ".")->string
+.
+calc> :l (new_from_string Chj::Path "./")->string
+./
+calc> :l (new_from_string Chj::Path "./")->clean->string
+./
+calc> :l (new_from_string Chj::Path "./..")->string
+./..
+calc> :l (new_from_string Chj::Path "./..")->clean->string
+..
+
+calc> :l (new_from_string Chj::Path "./foo/../bar/.//baz/.")->clean->string
+foo/../bar/baz/
+calc> :d (new_from_string Chj::Path "")->clean->string
+$VAR1 = '';
+# XX should this be an error?
+
+calc> :l (new_from_string Chj::Path ".")->string
+.
+calc> :d (new_from_string Chj::Path ".")->clean->string
+$VAR1 = './';
+
+calc> :l (new_from_string Chj::Path "/")->string
+/
+calc> :l (new_from_string Chj::Path "/")->clean->string
+/
+calc> :l (new_from_string Chj::Path "/.")->clean->string
+/
+calc> :l (new_from_string Chj::Path "/./")->clean->string
+/
+calc> :l (new_from_string Chj::Path "/./")->string
+/./
+calc> :l (new_from_string Chj::Path "/.")->string
+/.
