42f9c1eb82e03ea4ead9d66891cefa3583412c5a
Christian Jaeger (tie) <chrjae@gmail.com>
2012-06-25 23:18:04 +0100

    - (and if not in first position also +) don't need escaped symbols

--- Chj/Serial/Sexpr.pm
+++ Chj/Serial/Sexpr.pm
@@ -40,7 +40,7 @@ use strict;
 sub valid_keyword_or_symbol {
     my ($str)=@_;
     # what goes without escaping for Scheme
-    $str=~ /^[_a-zA-Z]\w*\z/
+    $str=~ /^[_a-zA-Z-][\w+-]*\z/
 }
 
 sub keyword_or_symbol_to_printstring {
