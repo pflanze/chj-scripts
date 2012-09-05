7caa789650f4c27230e77c3dea8bacef76567362
Christian Jaeger (tie) <chrjae@gmail.com>
2012-09-05 23:50:43 +0100

    add array_uniq

--- Chj/FP/ArrayLib.pm
+++ Chj/FP/ArrayLib.pm
@@ -27,7 +27,9 @@ Functions for arrays.
 package Chj::FP::ArrayLib;
 @ISA="Exporter"; require Exporter;
 @EXPORT=qw();
-@EXPORT_OK=qw(array_fold);
+@EXPORT_OK=qw(array_fold
+	      array_uniq
+	    );
 %EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
 
 use strict;
@@ -41,6 +43,21 @@ sub array_fold ( $ $ $ ) {
     $res
 }
 
+sub array_uniq ( $ $ ) {
+    my ($ary,$eq)=@_;
+    return [] unless @$ary;
+    my $v= $$ary[0];
+    my $res= [$v];
+    for (my $i=1; $i < @$ary; $i++) {
+	my $v2= $$ary[$i];
+	if (not &$eq ($v,$v2)) {
+	    $v= $v2;
+	    push @$res, $v;
+	}
+    }
+    $res
+}
+
 1
 
 __END__
@@ -49,3 +66,31 @@ calc> :l array_fold sub {my ($v,$r)=@_; $v+$r }, 0, [5,6,7]
 18
 calc> :l (array_fold \&cons, $Chj::FP::EmptyList, [5,6,7])->string
 (7 6 5)
+
+calc> :d array_uniq [ 1,2,3,3,4,6,9,9 ], \&number_eq
+$VAR1 = [
+          1,
+          2,
+          3,
+          4,
+          6,
+          9
+        ];
+calc> :d array_uniq [ 3,3,4,6,9,9 ], \&number_eq
+$VAR1 = [
+          3,
+          4,
+          6,
+          9
+        ];
+calc> :d array_uniq [ 3,3,9,9 ], \&number_eq
+$VAR1 = [
+          3,
+          9
+        ];
+calc> :d array_uniq [ 3,3,3 ], \&number_eq
+$VAR1 = [
+          3
+        ];
+calc> :d array_uniq [ ], \&number_eq
+$VAR1 = [];
