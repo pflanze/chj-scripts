bda83945e25fd6ddd62259465ceff9edf854e04a
root <ch@christianjaeger.ch>
2013-06-09 15:19:41 +0100

    defluff (hmm, stripped lots of info?)

--- Chj/Sendmail.pm
+++ Chj/Sendmail.pm
@@ -47,8 +47,6 @@ use Chj::singlequote;#debug only
 
 BEGIN {
     eval( ($] >= 5.008) ?
-#	  'sub has_hibit { Encode::is_utf8($_[0]) # && has some non ascii chars, how to check?
-#                            or $_[0] =~ /[\x80-\xFF]/s }'
 	  'sub has_hibit { my $len= length $_[0]; for (my $i=0; $i<$len; $i++) { if (ord(substr $_[0],$i,1) > 127) { return 1 }} 0 }'
 	  : 'use encoding "latin1";#(necessary for perl 5.6 legacy?)  (HMM will 5.6 only work with ISO-8859-1 anyway because of all that?)
              sub has_hibit { $_[0] =~ /[\x80-\xFF]/s }');
@@ -73,27 +71,17 @@ our $Syserror= sub {
     croak @_;
 };
 
-#use Data::Dumper;
-
 sub sendmail {
-#    warn "sendmail: ".Dumper(\@_);
     my $msg= &preparemail;
     if ($msg->send) {
 	#ok
     } else {
-#     if ($Mail::Sendmail::error=~ /\bno recipient\b/i) {
-#         $PermanentError->();
-#     } else {
-# 	$TemporaryError->();
-#     }
-	#ahh: hatte obiges schon länger auskommentiert  ? grr. "##kann keine genauere Meldung kriegen? Wenn qmail failure gibt: hat's was auf stderr gegeben? sigh."
 	$PermanentError->("Some error occured sending mail");
     }
 }
 
 sub preparemail {
     my @realargs;# array of references
-    #my $Encoding= nein das meint quoted-printable o.ä.
     my $Charset= 'ISO-8859-1'; # unless overridden
     my ($flag_Data,$flag_Encoding,$flag_ContentType);
     my $Data;
@@ -102,7 +90,6 @@ sub preparemail {
 	if ($$_ eq 'Data') {
 	    $flag_Data=1;
 	    $Data= encode($Charset,$_[$i+1]);
-	    # Fri, 12 May 2006 20:17:53 +0200: Text::Wrap seems to make crlf into the text, so..:
 	    $Data=~ s/\015\012/\n/sg;
 	    push @realargs, $_, \ $Data;
 	} elsif ($$_ eq 'Encoding') {
@@ -157,13 +144,11 @@ sub preparemail {
 	unshift @realargs, \ ("Encoding","quoted-printable");
     }
     #use Data::Dumper; warn __PACKAGE__.": realargs= ".Dumper( \@realargs);
-    my $msg= MIME::Lite->new(map { $$_} @realargs) or $Syserror->("Error creating mailer object");
-    if (!$flag_ContentType) {#(FRAGE vom 12.4.06: warum auf ContentType schauen für charset?. hm ja wohl: normalerweise gibt man es nicht; dann müssen wir hier; aber eigentlich müssten wir nur, wenn eben encoding wirklich passiert ist, right? todo)
-	#unshift @args, \ ("Content-Type","text/plain; charset=ISO-8859-1");
+    my $msg= MIME::Lite->new(map { $$_} @realargs)
+      or $Syserror->("Error creating mailer object");
+    if (!$flag_ContentType) {
+	#(why look at ContentType for charset?. (see githistory?))
 	$msg->attr('content-type.charset' => $Charset);
-	#warn "HABE es auf iso gesetzt!";
-	#use Data::Dumper;
-	#warn Dumper($msg);  alles normal  was ist los
     }
     $msg
 }
@@ -209,72 +194,4 @@ sub send_mailasstring {  # code borrowed from MIME::Lite
 }
 
 
-1;
-__END__
-
-=head1 BUGS
-
-MIME handling of Subject line is murksy:
-
-- if no 8 bit chars are present, then it is left unchanged. This means that if the given subject string contains +-correct encoded word mime markup (RFC 2047) then the mail reader will decode it. The plus of this behaviour is that one can pass a string from an incoming mail without decoding it first, i.e. "Re: $origsubject".
-
-- if however there is an 8 bit char, then everything is encoded, so if the subject contained some encoding it will be escaped and appear at the client's end as such.
-
-(Maybe there should be a flag/setting somewhere to switch between those two manually.)
-
-=cut
-
-# --------------------------------
-use Class::Array -fields=> qw(
-	
-); 
-
-#use vars qw();
-
-sub new {
-	my $class=shift;
-	my $self= $class->SUPER::new(@_);
-	# (Alternativ, wenn diese Klasse die Basisklasse ist und bleiben soll:)
-	# my $self = bless [], $class;
-	# çç wenn hier keine speziellen Aktionen nötig sind, das ganze new rauskippen.
-	$self
-}
-
-
-1;
-
-__END__
-
-Thu, 19 Dec 2002 02:18:25 +0100
-
-Shit, die mimelite version von send hat ein problem (links): bei local delivery, oder delivery an den sms gateway. Die lib nimmt From Wert als Argument fur sendmail irgendwie oder wie kommt das???:
-< Return-Path: <"Christian Jaeger <christian.jaeger"@ethlife.ethz.ch>>
----
-> Return-Path: <chris@ethlife-b.ethz.ch>
-
- Aha:
-
-        unless (exists($p{SetSender}) and !$p{SetSender}) {
-            my $from = $p{FromSender} || ($self->get('From'))[0];
-            if ($from) {
-                my ($from_addr) = extract_addrs($from);
-                push @cmd, "-f$from_addr"       if $from_addr;
-            }
-        }
-
-und
-  if (eval "require Mail::Address") {
-    push @Uses, "A$Mail::Address::VERSION";
-    eval q{
-        sub extract_addrs {
-            return map { $_->format } Mail::Address->parse($_[0]);
-        }
-    }; ### q
-
-PS. andrere bug ja eben :  
-
----
-
-  Workaround: ?.nee, besser library patchen.
-  Ah hem. Hier nun auch noch korrigiert. Komisch dass vorher nich fehler? From wohl nie mit <> gewesen?.
-  
+1
