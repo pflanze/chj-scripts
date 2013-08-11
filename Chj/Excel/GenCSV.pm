88934dbbddd56fb28e815a7f1823fc0731f55861
Christian Jaeger (novo) <ch@christianjaeger.ch>
2013-08-11 12:29:01 +0100

    cleanup

--- Chj/Excel/GenCSV.pm
+++ Chj/Excel/GenCSV.pm
@@ -1,4 +1,4 @@
-# Wed Jan 14 17:22:34 2004  Christian Jaeger, christian.jaeger@ethlife.ethz.ch
+# Wed Jan 14 17:22:34 2004  Christian Jaeger, ch@christianjaeger.ch
 # 
 # Copyright 2001 by ethlife renovation project people
 # Published under the terms of the GNU General Public License
@@ -13,33 +13,22 @@ Chj::Excel::GenCSV
 
 =head1 DESCRIPTION
 
-Funktionen bloss.
-
-EIGENTLICHNICHT CSV sondern text  tab  format  weil das das einzige ist was in Mac funktionniert.
+Functions to help generate CSV or TSV (tab separated columns) format files.
 
 =cut
 
 
 package Chj::Excel::GenCSV;
-#require "Exporter";#  HUH?: Exporter did not return a true value at /root/extlib/Chj/Excel/GenCSV.pm line 24.
-#use Exporter ();# geht. ??
-require Exporter;# ah.
-
-@ISA="Exporter";
+@ISA="Exporter"; require Exporter;
+@EXPORT=qw();
 @EXPORT_OK=qw(
 	      excel_datetime
 	      excel_unix2datetime
 	      excel_quote
 	      );
-use strict;
+%EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
 
-sub quote_csv {
-    my $str=shift;
-    $str=~ s/\\/\\\\/sg;
-    $str=~ s/"/\\"/sg;
-    #$str=~ s/,/\\,/sg; aber das isch doch nöd nötig? dafür sind doch anf zeichen.
-    "\"$str\""
-}
+use strict;
 
 sub quote {
     my $str=shift;
@@ -59,7 +48,8 @@ sub datetime {
     }
 }
 *excel_datetime= \&datetime;
-sub unix2datetime {# sollte halt WIRKLICH ne zeit klasse sein  von mir oder so. ?
+
+sub unix2datetime {
     my $unix=shift;
     my ($year,$monthno,$mday,$hour,$min,$sec)=(localtime $unix)[5,4,3,2,1,0];
     $year+=1900;
@@ -69,4 +59,4 @@ sub unix2datetime {# sollte halt WIRKLICH ne zeit klasse sein  von mir oder so. 
 }
 *excel_unix2datetime= \&unix2datetime;
 
-1;
+1
