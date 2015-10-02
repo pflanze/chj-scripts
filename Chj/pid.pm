f4bf7f8a7a2daa633cb4429336c8aeb7add48f75
Christian Jaeger (novo) <ch@christianjaeger.ch>
2015-10-02 22:55:02 +0100

    pid, time: exported variables can't be localized, provide accessors
    huh?

--- Chj/pid.pm
+++ Chj/pid.pm
@@ -12,12 +12,21 @@ Chj::pid - overridable process id
  use Chj::pid; # exports 'pid'
 
  my $pid= pid; # same as $$
- use Chj::pid '$pid';
- local $pid= 1234;
- my $pid1= pid; # 1234
+
+ use Chj::pid qw(pid set_pid);
+ set_pid 1234;
+ # or dynamically scoped:
+ with_pid 1234, sub {
+     my $pid1= pid; # 1234
+ };
 
 =head1 DESCRIPTION
 
+Overridable process id value for deterministic testing.
+
+=head1 SEE ALSO
+
+L<Chj::time>
 
 =cut
 
@@ -25,7 +34,7 @@ Chj::pid - overridable process id
 package Chj::pid;
 @ISA="Exporter"; require Exporter;
 @EXPORT=qw(pid);
-@EXPORT_OK=qw($pid);
+@EXPORT_OK=qw($pid set_pid);
 %EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
 
 use strict; use warnings FATAL => 'uninitialized';
@@ -34,6 +43,16 @@ our $pid;
 
 sub pid () {
     $pid // $$
+}
+
+sub set_pid ($) {
+    ($pid)=@_;
+}
+
+sub with_pid ($$) {
+    my($v,$thunk)=@_;
+    local $pid=$v;
+    &$thunk()
 }
 
 1
