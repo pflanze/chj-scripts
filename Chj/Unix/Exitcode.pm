b8ebde4f093c4d8b67f400c0a5e9b55f963e069f
Christian Jaeger <ch@christianjaeger.ch>
2020-05-08 13:40:51 +0100

    Merge Chj/Unix/{exitcode,Exitcode}.pm into one file
    Mainly to avoid issue on case insensitive filesystems.

--- Chj/Unix/Exitcode.pm
+++ Chj/Unix/Exitcode.pm
@@ -1,6 +1,6 @@
 # Sat Aug  4 09:38:07 2007  Christian Jaeger, christian at jaeger mine nu
 # 
-# Copyright 2007 by Christian Jaeger
+# Copyright (c) 2007-2019 Christian Jaeger, copying@christianjaeger.ch
 # Published under the same terms as perl itself
 #
 # $Id$
@@ -18,37 +18,52 @@ Chj::Unix::Exitcode
 
 
 package Chj::Unix::Exitcode;
+@ISA="Exporter"; require Exporter;
+@EXPORT=qw(exitcode);
+@EXPORT_OK=qw(exitcode);
+%EXPORT_TAGS=(all=>\@EXPORT_OK);
 
 use strict; use warnings; use warnings FATAL => 'uninitialized';
 
-use Chj::Unix::Signal;
+package Chj::Unix::Exitcode::Exitcode {
 
-use Class::Array -fields=>
-  -publica=>
-  'code',
-  ;
+    use Chj::Unix::Signal;
+
+    use Class::Array -fields=>
+      -publica=>
+      'code',
+      ;
 
 
-sub new {
-    my $class=shift;
-    my $s= $class->SUPER::new;
-    ($$s[Code])=@_;
-    $s
-}
+    sub new {
+        my $class=shift;
+        my $s= $class->SUPER::new;
+        ($$s[Code])=@_;
+        $s
+    }
 
-sub as_string {
-    my $s=shift;
-    my $code= $$s[Code];
-    if ($code < 256) {
-        "signal $code (".Chj::Unix::Signal->new($code)->as_string.")"
-    } else {
-        if (($code & 255) == 0) {
-            "exit value ".($code >> 8)
+    sub as_string {
+        my $s=shift;
+        my $code= $$s[Code];
+        if ($code < 256) {
+            "signal $code (".Chj::Unix::Signal->new($code)->as_string.")"
         } else {
-            warn "does this ever happen?";
-            "both exit value and signal ($code)"
+            if (($code & 255) == 0) {
+                "exit value ".($code >> 8)
+            } else {
+                warn "does this ever happen?";
+                "both exit value and signal ($code)"
+            }
         }
     }
+
+    end Class::Array;
 }
 
-end Class::Array;
+
+sub exitcode ( $ ) {
+    my ($code)=@_;
+    Chj::Unix::Exitcode::Exitcode->new($code)->as_string;
+}
+
+1
