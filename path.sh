96f308bc69816800690e5a1d04a35e27c9ba32ba
Christian Jaeger <ch@christianjaeger.ch>
2021-02-14 16:04:48 +0000

    path.sh: make sh compatible and provide PATH_add

--- path.sh
+++ path.sh
@@ -1,4 +1,11 @@
 # Add this directory to PATH if not there already.
-if ! [[ :$PATH: = *:/opt/chj/bin:* ]]; then
-    PATH=/opt/chj/bin:$PATH
-fi
+
+function PATH_add {
+    local dir="$1"
+    case ":$PATH:" in
+        *:"$dir":*) ;;
+        *) PATH=$dir:$PATH;
+    esac
+}
+
+PATH_add /opt/chj/bin
