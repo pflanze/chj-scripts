672bf391f7c89f620f8e19a61f13ddc90e257b2c
Christian Jaeger <ch@christianjaeger.ch>
2021-05-05 12:58:40 +0100

    Xml2Sexpr: cleanup

--- Chj/Transform/Xml2Sexpr.pm
+++ Chj/Transform/Xml2Sexpr.pm
@@ -1,9 +1,8 @@
 # Mon Jun 12 02:50:56 2006  Christian Jaeger, christian.jaeger@ethlife.ethz.ch
 # 
-# Copyright 2004 by Christian Jaeger
+# Copyright 2004-2021 by Christian Jaeger
 # Published under the same terms as perl itself
 #
-# $Id$
 
 =head1 NAME
 
@@ -31,56 +30,39 @@ use strict;
 use utf8;
 
 use XML::LibXML;
-#use Chj::xopen qw(xopen_
 use Chj::xtmpfile;
 use Chj::schemestring;
 
-my $new_perl= ($] >= 5.008);
-sub outencode {
-    @_
-#     if ($new_perl) {
-# 	@_
-#     } else {
-# 	map {
-# 	    #XML::LibXML::encodeToUTF8($_)  ochnein geht nicht. weil quell encoding unknown.
-# 	    die "sh?"
-# 	} @_
-#     }
-      #mensch: war gar nicht hier das problem mit dem alten perl. sondern latin1 *ausgabe* von scheme her. und input auch.grr
-}
-
 my $parser= XML::LibXML->new;
-$parser->load_ext_dtd(0); # !!!  and they don't even say that it was on by default?
-#URGH but STILL NO HELP.
-$parser->validation(0); #doesn't help either.
+# Try to avoid loading DTD, does not help?
+$parser->load_ext_dtd(0);
+$parser->validation(0);
 
 
 sub walk_element {
     my ($node,$o)=@_;
-    xprint $o "(";
-    my $elname= $node->nodeName;#?
-    xprint $o outencode($elname);#
-    #my $attrnode = $node->getAttributeNode ( $aname ); #eeehr  und wie krieg ich die namen ?
-    #ah: @attrs =$node->attributes;  von Node Klasse. sowas echt komisches wenn dann doch alle nodes egalwas attribute haben können.  ?.
+    $o->xprint("(");
+    my $elname= $node->nodeName;
+    $o->xprint($elname);
     if (my @attrs=$node->attributes) {
-	xprint $o "(@";
+	$o->xprint("(@");
 	for my $attr (@attrs) {
-	    xprint $o outencode("(".($attr->name)." ".schemestring($attr->value).")");
+	    $o->xprint("(".($attr->name)." ".schemestring($attr->value).")");
 	}
-	xprint $o ")";
+	$o->xprint(")");
     }
     for my $child ($node->childNodes) {
 	if ($child->isa("XML::LibXML::Element")) {
 	    walk_element( $child,$o);
 	} elsif ($child->isa("XML::LibXML::Comment")) {
-	    xprint $o "(*COMMENT*" , outencode( schemestring( $child->data)) , ")";
+	    $o->xprint("(*COMMENT*", schemestring( $child->data) , ")");
 	} elsif ($child->isa("XML::LibXML::Text")){
-	    xprint $o outencode( schemestring( $child->data));
+	    $o->xprint(schemestring( $child->data));
 	} else {
 	    die "unknown child type: $child" ##
 	}
     }
-    xprint $o ")";
+    $o->xprint(")");
 }
 
 sub file_xml_to_sexpr {
@@ -92,18 +74,10 @@ sub file_xml_to_sexpr {
     my $tree= $parser->parse_file($filepath);##or die ?
 
     my $o= xtmpfile $out;
-    if ($new_perl) {
-	binmode($o,":utf8") or die "binmode: $!";
-    } else {
-	#
-    }
-    #walk
-    #selber wieder mal ?
+    binmode($o,":utf8") or die "binmode: $!";
     my $top= $tree->documentElement  or die;
     walk_element $top,$o;
     $o->xclose;
-    #$o->xputback;##oderso.
-    #$o->xreplace_or_withmode  ist für wenn ziel anders ist als eigener path.
     $o->xputback(0666);
 }
 
@@ -111,11 +85,7 @@ sub STRING_OR_PORT_xml_to_sexpr {
     my ($method, $string_or_port, $opt_outport)=@_;
     my $o= $opt_outport || do {
 	my $o= bless *STDOUT{IO},"Chj::IO::File";
-	if ($new_perl) {
-	    binmode($o,":utf8") or die "binmode: $!";
-	} else {
-	    #
-	}
+        binmode($o,":utf8") or die "binmode: $!";
 	$o
     };
     my $tree= $parser->$method($string_or_port);##or die ?
